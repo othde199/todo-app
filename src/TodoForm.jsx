3609af7ace14d555344356878c70e26d59c95e1d
othde199 <126416690+othde199@users.noreply.github.com>
2026-09-17 21:58:44 +0330

    feat(bot): نوشته lets go به LG تبدیل شد

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -71,7 +71,7 @@ function TodoForm() {
                 onReorder={handleReorder}
             />
             <form onSubmit={handleAddTodo} action="" className="todo-form">
-                <label htmlFor="todo-input">lets go</label>
+                <label htmlFor="todo-input">LG</label>
                 <input
                     type="text"
                     id="todo-input"
